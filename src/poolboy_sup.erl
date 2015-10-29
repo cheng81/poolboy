bd7249ef676bc7f9a9638dc35e85b4db5557d216
Francesco Zanitti <fzanitti@gmail.com>
2015-10-29 13:57:49 +0100

    removed double-list for worker arguments. retrofit poolboy:start and start_link to make the tests pass

--- src/poolboy_sup.erl
+++ src/poolboy_sup.erl
@@ -8,7 +8,8 @@
 start_link(Mod, Args) ->
     supervisor:start_link(?MODULE, {Mod, Args}).
 
-init({Mod, Args}) ->
+init({Mod, Args}) when is_list(Args) ->
     {ok, {{simple_one_for_one, 0, 1},
-          [{Mod, {Mod, start_link, [Args]},
-            temporary, 5000, worker, [Mod]}]}}.
+          [{Mod, {Mod, start_link, Args},
+            temporary, 5000, worker, [Mod]}]}};
+init({Mod, Args}) -> init({Mod, [Args]}).
